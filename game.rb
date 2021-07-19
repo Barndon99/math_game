ec9d92bb1bf588d0475b3e66a5d206a2b8608773
Brandon Rowlandson <rowlandsonbrandon@gmail.com>
2021-07-19 17:41:05 -0600

    Finish game logic

--- game.rb
+++ game.rb
@@ -10,6 +10,25 @@ class Game
     @player_turn = @players[0]
   end
 
+  def wrong_answer
+    if @player_turn == @players[0] 
+      @players[1].lives -= 1
+      if @players[1].lives == 0
+        abort("#{@players[1].name}, you lose! \n ----- GAME ENDED -----")
+      end
+    end
+    if @player_turn == @players[1] 
+      @players[0].lives -= 1
+      if @players[0].lives == 0
+        abort("#{@players[0].name}, you lose! \n ----- GAME ENDED -----")
+      end
+    end
+  end
+
+  def current_score
+    puts "The current score is #{@players[0].name}: #{@players[0].lives}/3 to #{@players[1].name}: #{@players[1].lives}/3!"
+  end
+
   def change_player_turn
     if @player_turn == @players[0]
       @player_turn = @players[1]
@@ -18,19 +37,38 @@ class Game
     end
   end
 
-  def wrong_answer
-    @player_turn.lives -= 1
-    if @player_turn.lives == 0
-      puts "#{@player_turn.name}, you lose! \n ----- GAME ENDED -----" 
-    end
-  end
-
-  def current_score
-    puts "The current score is #{@players[0].name}: #{@players[0].lives}/3 to #{@players[1].name}: #{@players[1].lives}/3!"
-  end
-
   def inc_rounds
     @round += 1
+  end
+
+  def start
+    while self.players[0].lives != 0 || self.players[1].lives != 0 do
+      puts "----- Round #{self.round} -----"
+      self.inc_rounds
+    
+      self.change_player_turn
+    
+      puts "First number?"
+      num1 = gets.chomp.to_i
+      puts "Second number?"
+      num2 = gets.chomp.to_i
+    
+      question = Question.new(num1, num2)
+    
+      puts "#{self.player_turn.name}: #{question.question}"
+    
+      answer = gets.chomp.to_i
+    
+      if (answer == question.answer)
+        puts "Correct!"
+      end
+    
+      if (answer != question.answer)
+        self.wrong_answer
+      end
+    
+      puts self.current_score
+    end
   end
 
 end
--- main.rb
+++ main.rb
@@ -12,31 +12,4 @@ player1 = Player.new(name1)
 
 game = Game.new(player0, player1)
 
-unless player0.lives == 0 || player1.lives == 0
-  puts "----- Round #{game.round} -----"
-
-  puts "First number?"
-  num1 = gets.chomp.to_i
-  puts "Second number?"
-  num2 = gets.chomp.to_i
-
-  question = Question.new(num1, num2)
-
-  puts "#{game.player_turn.name}: #{question.question}"
-
-  answer = gets.chomp.to_i
-
-  if (answer == question.answer)
-    puts "Correct!"
-  end
-
-  if (answer != question.answer)
-    game.wrong_answer
-  end
-
-  puts game.current_score
-
-  game.inc_rounds
-
-  game.change_player_turn
-end
+game.start
