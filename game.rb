a10f1d6caf077f8926b960a1d5f72cbb69ad2466
Brandon Rowlandson <rowlandsonbrandon@gmail.com>
2021-07-19 15:49:57 -0600

    Collect player names within main.rb

--- game.rb
+++ game.rb
@@ -1,9 +1,10 @@
 class Game
 #Initialize collect names for players
+  attr_accessor :players, :round, :player_turn
   def initialize(player1, player2)
-    @Players = [player1, player2]
-    @Round = 0
-    @Player_turn = @Players[@Round]
+    @players = [player1, player2]
+    @round = 0
+    @player_turn = @players[@round]
   end
 
 end
--- main.rb
+++ main.rb
@@ -1,3 +1,12 @@
-require '/game.rb'
-require '/player.rb'
-require '/question.rb'
+require "./game.rb"
+require "./player.rb"
+require "./question"
+
+puts "Enter name Player 1: "
+player1 = gets.chomp.to_s
+puts "Enter name Player 2: "
+player2 = gets.chomp.to_s
+
+game = Game.new(player1, player2)
+
+puts game.players
